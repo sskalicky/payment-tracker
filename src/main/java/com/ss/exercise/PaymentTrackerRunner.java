ce2d2f6ec6aaca6c35b6efe86d926e2ce6e7e4e5
Stanislav Skalicky <skalicky.s@gmail.com>
2013-11-03 20:24:34 +0100

    payment-tracker - initial project setup

---
+++ src/main/java/com/ss/exercise/PaymentTrackerRunner.java
@@ -0,0 +1,12 @@
+package com.ss.exercise;
+
+public class PaymentTrackerRunner
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello PaymentTracker!" );
+	    for(String arg: args){
+		    System.out.println("arg:" + arg);
+	    }
+    }
+}
